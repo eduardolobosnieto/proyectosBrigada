2c9bba22c9622d1fac3d564cd9c5af0f34677d73
Eduardo <eduardolobosnieto@gmail.com>
2023-11-02 09:26:22 -0300

    Modal para editar
    Se agregan hidden para pasar variables a editProy.php

--- app/editProyModal.php
+++ app/editProyModal.php
@@ -8,28 +8,35 @@
       </div>
       <div class="modal-body">
 
-        <form action="#" method="post">
+        <form action="editProy.php" method="post">
+
           <div class="mb-3">
-            <input type="hidden" id="rut" name="rut" value="<?= $rut; ?>"/>
+          <input type="hidden" id="rut" name="rut" value="<?= $rut; ?>"/>
             <label for="nombre" class="form-label">Nombre del Proyecto: </label>
             <input type="text" name="nombre" id="nombre" class="form-control" value="<?php echo $rowProy1['2'];?>" disabled>
+            <input type="hidden" id="id_proyecto" name="id_proyecto" value="<?= $rowProy1['0'];?>">
           </div>
+
           <div class="mb-3">
             <label for="descripcion" class="form-label">Descripcion: </label>
             <textarea type="text" name="descripcion" id="descripcion" class="form-control" rows="3" required><?= $rowProy1['7'];?></textarea>
+            <input type="hidden" id="descripcion_old"name="descripcion_old" value="<?php echo $rowProy1['7'];?>">
           </div>
+
           <div class="mb-3">
             <label for="nombre" class="form-label">Version del Proyecto: <?php echo $rowProy1['9'];?></label>
+            <input type="hidden" id="version"name="version" value="<?php echo $rowProy1['9'];?>">
             <br/>
             <label for="nombre" class="form-label">Estado del Proyecto: <?php echo $rowProy1['10'];?></label>
             <br/>
           </div>
+
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="far fa-times-circle"></i> Cerrar</button>
           <button type="submit" class="btn btn-primary"><i class="far fa-edit"></i> Editar</button>
 
 
         </form>
-      
+
       </div>
 
     </div>
